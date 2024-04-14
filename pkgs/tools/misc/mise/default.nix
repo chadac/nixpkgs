c462fedd2c9548d62a4a5464eab4f891d6054c7f
Chad Crawford <chad@cacrawford.org>
2024-04-14 02:01:57 -0400

    rename rtx to mise

--- pkgs/tools/misc/mise/default.nix
+++ pkgs/tools/misc/mise/default.nix
@@ -11,22 +11,22 @@
 , direnv
 , Security
 , SystemConfiguration
-, rtx
+, mise
 , testers
 }:
 
 rustPlatform.buildRustPackage rec {
-  pname = "rtx";
-  version = "2023.12.35";
+  pname = "mise";
+  version = "2024.1.0";
 
   src = fetchFromGitHub {
     owner = "jdx";
-    repo = "rtx";
+    repo = "mise";
     rev = "v${version}";
-    hash = "sha256-vzMjC6qIPhZm80hzYQRpF3j+s85B0nwTcgSGRATQEIg=";
+    hash = "sha256-v8GQzDMyCGfFEsxG0H7myGTWDX+O8S8C9L0KJ267r9U=";
   };
 
-  cargoHash = "sha256-LvW5xGVggzuXlFPhbrc93Dht3S9zaQyx9Nm+Mx/Mjh0=";
+  cargoHash = "sha256-mwV12QA4Fe+ueu8Z6GU0f0siETQqWUMc2eUEURT2Lok=";
 
   nativeBuildInputs = [ installShellFiles pkg-config ];
   buildInputs = [ openssl ] ++ lib.optionals stdenv.isDarwin [ Security SystemConfiguration ];
@@ -46,30 +46,30 @@ rustPlatform.buildRustPackage rec {
     # Requires .git directory to be present
     "--skip=cli::plugins::ls::tests::test_plugin_list_urls"
   ];
-  cargoTestFlags = [ "--all-features" ];
+  cargoTestFlags = [ "--all-features" "--bin mise" ];
   # some tests access the same folders, don't test in parallel to avoid race conditions
   dontUseCargoParallelTests = true;
 
   postInstall = ''
-    installManPage ./man/man1/rtx.1
+    installManPage ./man/man1/mise.1
 
     installShellCompletion \
-      --bash ./completions/rtx.bash \
-      --fish ./completions/rtx.fish \
-      --zsh ./completions/_rtx
+      --bash ./completions/mise.bash \
+      --fish ./completions/mise.fish \
+      --zsh ./completions/_mise
   '';
 
   passthru = {
     updateScript = nix-update-script { };
-    tests.version = testers.testVersion { package = rtx; };
+    tests.version = testers.testVersion { package = mise; };
   };
 
   meta = {
-    homepage = "https://github.com/jdx/rtx";
-    description = "Polyglot runtime manager (asdf rust clone)";
-    changelog = "https://github.com/jdx/rtx/releases/tag/v${version}";
+    homepage = "https://github.com/jdx/mise";
+    description = "dev tools, env vars, task runner";
+    changelog = "https://github.com/jdx/mise/releases/tag/v${version}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ konradmalik ];
-    mainProgram = "rtx";
+    mainProgram = "mise";
   };
 }
